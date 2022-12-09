834623170cc2e5c405f31b9a088cc0be38135e62
Priyanshu Bhandari <technopriyu@gmail.com>
2022-12-09 15:03:48 +0530

    added news api calls with stepzen and gql

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import fetchNews from '../lib/fetchNews';
 
 const Homepage = async () => {
   // fetch news data
-  // const news: NewsResponse = await fetchNews(categories.join(','));
+  const news: NewsResponse = await fetchNews(categories.join(','));
+  console.log(news);
   return <div className=""></div>;
 };
 export default Homepage;
--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,12 +1,78 @@
+import { gql } from 'graphql-request';
+import sortNewsByImage from './sortNewsByImage';
+
 const fetchNews = async (
   category?: Category | string,
   keywords?: string,
   isDynamic?: boolean
 ) => {
-  // GraphQL Query
+  const query = gql`
+    query MyQuery(
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "in"
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
+        data {
+          author
+          category
+          country
+          description
+          image
+          language
+          published_at
+          source
+          title
+          url
+        }
+        pagination {
+          count
+          offset
+          total
+          limit
+        }
+      }
+    }
+  `;
+
   // Fetch with caching...
+  const res = await fetch(
+    'https://tolu.stepzen.net/api/scaramouche/__graphql',
+    {
+      method: 'POST',
+      // no-cache means SSR
+      cache: isDynamic ? 'no-cache' : 'default',
+      // if not dynamic then revalidate it 20s
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          access_key: process.env.MEDIASTACK_API_KEY,
+          categories: category,
+          keywords: keywords,
+        },
+      }),
+    }
+  );
+
+  console.log('Loading NEW DATA FROM API for category ->', category, keywords);
+
+  const newsResponse = await res.json();
+  console.log(newsResponse);
   // Sort by images vs not images present
-  // return;
+  const news = sortNewsByImage(newsResponse.data.myQuery);
+
+  return news;
 };
 
 export default fetchNews;
